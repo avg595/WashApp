e81b653759227d74adcd61e4487e7fed6234709c
andreivg1995 <cf17andrei.gindac@iesjoandaustria.org>
2020-10-22 18:23:02 +0200

    Customer

--- spring-backend/src/main/java/net/javaguides/springboot/model/Customer.java
+++ spring-backend/src/main/java/net/javaguides/springboot/model/Customer.java
@@ -8,12 +8,18 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "employees")
-public class Employee {
+@Table(name = "customers")
+public class Customer {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "user_name")
+	private String userName;
 	
 	@Column(name = "email")
 	private String email;
@@ -21,35 +27,57 @@ public class Employee {
 	@Column(name = "password")
 	private String password;
 	
-	public Employee() {
+	public Customer() {
 		
 	}
-	
-	public Employee(long id, String email, String password) {
+
+	public Customer(long id, String name, String userName, String email, String password) {
 		super();
 		this.id = id;
+		this.name = name;
+		this.userName = userName;
 		this.email = email;
 		this.password = password;
 	}
-	
+
 	public long getId() {
 		return id;
 	}
+
 	public void setId(long id) {
 		this.id = id;
 	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
 	
 }
