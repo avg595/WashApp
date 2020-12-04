8db27c6278744ea9019a4fe9e8f277dfcf3bdb7e
andreivg1995 <cf17andrei.gindac@iesjoandaustria.org>
2020-12-04 08:57:34 +0100

    Product

---
+++ spring-backend/src/main/java/net/javaguides/springboot/controller/ProductController.java
@@ -0,0 +1,48 @@
+package net.javaguides.springboot.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.javaguides.springboot.exception.ResourceNotFoundException;
+import net.javaguides.springboot.model.Product;
+import net.javaguides.springboot.repository.ProductRepository;
+
+@RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/api/v3")
+public class ProductController {
+
+	@Autowired
+	private ProductRepository productRepository;
+	
+	// get all products
+	@GetMapping("/products")
+	public List<Product> getAllProducts() {
+		return productRepository.findAll();
+	}
+
+	// create product rest api
+	@PostMapping("/products")
+	public Product createProducts(@RequestBody Product product) {
+		return productRepository.save(product);
+	}
+
+	// get product by id
+	@GetMapping("products/{id}")
+	public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+
+		Product product = productRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Product not exist with id : " + id));
+
+		return ResponseEntity.ok(product);
+	}
+}
---
+++ spring-backend/src/main/java/net/javaguides/springboot/model/Product.java
@@ -0,0 +1,82 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "products")
+public class Product {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "price")
+	private float price;
+	
+	@Column(name = "type")
+	private int type;
+	
+	@Column(name = "src")
+	private String src;
+
+	public Product() {
+		
+	}
+	
+	public Product(long id, String name, float price, int type, String src) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.price = price;
+		this.type = type;
+		this.src = src;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public float getPrice() {
+		return price;
+	}
+
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+	public int getType() {
+		return type;
+	}
+
+	public void setType(int type) {
+		this.type = type;
+	}
+
+	public String getSrc() {
+		return src;
+	}
+
+	public void setSrc(String src) {
+		this.src = src;
+	}
+}
